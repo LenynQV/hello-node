e6bf816437fd5178586df665f9bd1620c0d8bdba
LenynQV <lenyn.quiroga@gmail.com>
2018-12-15 11:06:38 -0500

    hello Lenyn

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('hello, world');
+  res.send('hello, Lenyn');
 });
 
 app.listen(process.env.PORT || 5000);
 
-if (process.env.PORT === undefined) {
-  console.log('Running at: http://localhost:5000');
-}
